38d7a73cd9bf718326f544fecfdf0b174ddb42af
Carlmarksjunior <plusiknoisy98@gmail.com>
2025-04-14 18:36:24 +0300

    2 коммит для fix

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,6 @@ public class Main {
         System.out.println("2-оой каммит");
         System.out.println("asdasdasd");
         System.out.println("1 коммит для fix");
+        System.out.println("2 коммит для fix");
     }
 }
