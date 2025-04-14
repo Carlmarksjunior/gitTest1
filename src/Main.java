3c6cd5bcaf1f792155f67d26c4357e273afe3dcc
Carlmarksjunior <plusiknoisy98@gmail.com>
2025-04-14 18:41:45 +0300

    fix после слияния

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,6 @@ public class Main {
         for (int i =0 ; i<=10; ++i){
             System.out.println(i);
         }
-        System.out.println("2-оой каммит");
         System.out.println("asdasdasd");
         System.out.println("1 коммит для fix");
         System.out.println("2 коммит для fix");
